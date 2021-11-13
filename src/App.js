b3d959ea57eab531e9d08f8c0ae0b25e3ce43801
R-Harsha-Raj <renukuntaharsharaj@gmail.com>
2021-11-13 15:44:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import "./App.css";
+import PreNavbar from "./component/PreNavbar";
+import Navbar from "./component/Navbar.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Slider from "./component/Slider.js";
+import data from "./data/data.json";
+import Offers from "./component/Offers.js";
+import Heading from "./component/Heading.js";
+import StarProduct from "./component/StarProduct.js";
+import HotAccessoriesMenu from "./component/HotAccessoriesMenu.js";
+import HotAccessories from "./component/HotAccessories.js";
+import ProductReviews from "./component/ProductReviews.js";
+import Videos from "./component/Videos.js";
+import Banner from "./component/Banner.js";
+import Footer from "./component/Footer.js";
+import NavOptions from "./component/NavOptions.js";
+
+function App() {
+  return (
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <NavOptions
+        miPhones={data.miPhones}
+        redmiPhones={data.redmiPhones}
+        tv={data.tv}
+        laptop={data.laptop}
+        fitnessAndLifeStyle={data.fitnessAndLifeStyle}
+        home={data.home}
+        audio={data.audio}
+        accessories={data.accessories}
+      />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Routes>
+        <Route
+          exact
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={data.hotAccessories.smartDevice}
+              smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/home"
+          element={
+            <HotAccessories
+              home={data.hotAccessories.home}
+              homeCover={data.hotAccessoriesCover.home}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/lifestyle"
+          element={
+            <HotAccessories
+              lifestyle={data.hotAccessories.lifeStyle}
+              lifestyleCover={data.hotAccessoriesCover.lifeStyle}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={data.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={
+                data.hotAccessoriesCover.mobileAccessories
+              }
+            />
+          }
+        />
+      </Routes>
+      <Heading text="PRODUCT REVIEWS" />
+
+      <ProductReviews productReviews={data.productReviews} />
+
+      <Heading text="VIDEOS" />
+
+      <Videos videos={data.videos} />
+
+      <Heading text="IN THE PRESS" />
+
+      <Banner banner={data.banner} />
+      <Footer footer={data.footer} />
+    </Router>
+  );
+}
+
+export default App;
